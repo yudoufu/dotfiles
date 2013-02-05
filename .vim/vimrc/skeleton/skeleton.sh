fbcab39d739b7681815ce97ef8c2ad657ff79dee
Daichi Kamemoto <daikame@gmail.com>
2013-02-05 13:07:46 +0900

    fix shell skeleteon

--- .vim/vimrc/skeleton/skeleton.sh
+++ .vim/vimrc/skeleton/skeleton.sh
@@ -11,16 +11,50 @@ usage() {
     exit 1;
 }
 
-OPTS=`getopt -o ho: -l help,option:, -- "$@"`
-eval set -- "$OPTS"
-while [ -n "$1" ]; do
-    case $1 in
-        -h|--help) usage;;
-        -o|--option) OPTION=$2; shift;;
-        --) break;;
-        *) usage;;
-    esac
-    shift
-done
+main() {
+    script_dir=$(cd $(dirname $0); pwd)
+    opts=`getopt -o ho: -l help,option:, -- "$@"`
+    eval set -- "$opts"
+    while [ -n "$1" ]; do
+        case $1 in
+            -h|--help) usage;;
+            -o|--option) option=$2; shift;;
+            --dry) is_dry=1;;
+            --) break;;
+            *) usage;;
+        esac
+        shift
+    done
 
+    if [ $is_dry ];then
+        echo "dry run..."
+    fi
+}
 
+## utility
+red() {
+    echo "[1;31m$1[0m"
+}
+
+yellow() {
+    echo "[1;33m$1[0m"
+}
+
+green() {
+    echo "[1;32m$1[0m"
+}
+
+fatal() {
+    red "$1"
+}
+
+warn() {
+    yellow "$1"
+}
+
+info() {
+    green "$1"
+}
+
+# call main.
+main "$@"
