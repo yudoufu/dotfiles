52313a2c3bdf318cdec7adeda9fe410fbcd1fe50
Daichi Kamemoto <daikame@gmail.com>
2014-12-20 01:23:52 +0900

    temporary add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+RUBY_VERSION=2.1.4
+PYTHON_VERSION=3.4.2
+PERL_VERSION=5.21.5
+NODE_VERSION=0.10.33
 
 usage() {
     echo "USAGE: `basename $0` [Options]"
@@ -8,18 +13,21 @@ usage() {
     echo ""
     echo "Options:"
     echo "  -h, --help          show this help."
+    echo "  -v, --verbose       show detail commands."
     echo "  --dry               dry run mode."
     exit 1;
 }
 
 main() {
+    real_script_dir=$(dirname $(readlink -f $0))
     script_dir=$(cd $(dirname $0); pwd)
-    opts=`getopt -o ho: -l help,option:,dry, -- "$@"`
+    opts=`getopt -o hvo: -l help,verbose,option:,dry, -- "$@"`
     eval set -- "$opts"
     while [ -n "$1" ]; do
         case $1 in
             -h|--help) usage;;
             -o|--option) option=$2; shift;;
+            -v|--verbose) is_verbose=1;;
             --dry) is_dry=1;;
             --) shift; break;;
             *) usage;;
@@ -33,11 +41,28 @@ main() {
 
     # implement here. #
 
+    install_langenv
+    otherenv
+    make_lang
+}
+
+install_langenv() {
+    install_rbenv
+    install_pyenv
+    install_plenv
+    install_nvm
+}
+
+make_lang() {
+    make_ruby $RUBY_VERSION
+    make_python $PYTHON_VERSION
+    make_perl $PERL_VERSION
+    make_node $NODE_VERSION
 }
 
 install_rbenv() {
-    path=$prefix/.rbenv
-    build_path=$path/plugins/ruby-build
+    local path=$prefix/.rbenv
+    local build_path=$path/plugins/ruby-build
     git clone git://github.com/sstephenson/rbenv.git $path
     git clone git://github.com/sstephenson/ruby-build.git $build_path
     echo "export PATH=$path/bin:\$PATH" >> $local_profile
@@ -45,8 +70,8 @@ install_rbenv() {
 }
 
 install_pyenv() {
-    path=$prefix/.pyenv
-    venv_path=$path/plugins/python-virtualenv
+    local path=$prefix/.pyenv
+    local venv_path=$path/plugins/python-virtualenv
     git clone git://github.com/yyuu/pyenv.git $path
     git clone git://github.com/yyuu/python-virtualenv.git $venv_path
     echo "export PATH=$path/bin:\$PATH" >> $local_profile
@@ -54,42 +79,63 @@ install_pyenv() {
 }
 
 install_plenv() {
-    path=$prefix/.plenv
+    local path=$prefix/.plenv
     git clone git://github.com/tokuhirom/plenv.git $path
     echo "export PATH=$path/bin:\$PATH" >> $local_profile
     echo 'eval "$(plenv init -)"' >> $local_profile
 }
 
 install_nvm() {
-    path=$prefix/.nvm
+    local path=$prefix/.nvm
     git clone git://github.com/creationix/nvm.git $path
-    echo 'source $(NVM_PATH)/nvm.sh' >> $local_profile
+    echo "source $path/nvm.sh" >> $local_profile
 }
 
-install_ruby() {
+make_ruby() {
+    local version=$1
+    rbenv install $version
+    cd $prefix; rbenv local $version
+    rbenv exec gem install bundler --no-ri --no-rdoc
+    rbenv rehash
 }
 
-install_python() {
+make_python() {
+    local version=$1
+    pyenv install $version
+    cd $prefix; pyenv local $version
+    pyenv rehash
 }
 
-install_perl() {
+make_perl() {
+    local version=$1
+    plenv install $version
+    cd $prefix; pyenv local $version
+    plenv install-cpan
+    plenv rehash
 }
 
-install_node() {
+make_node() {
+    local version=$1
+    nvm install v$version
+    nvm use v$verbose
+    nvm alias default v$version
 }
 
-set_otherenv() {
-  echo "export MAKEOPTS='-j $(expr `cat /proc/cpuinfo |grep processor |wc -l` + 2)'" >> $local_profile
+otherenv() {
+    mkdir -p $HOME/bin
+	echo 'export PATH="$HOME/bin:$PATH"' >> $local_profile
+	echo "export MAKEOPTS=\"-j $(shell expr `cat /proc/cpuinfo |grep processor |wc -l` + 2)\"" >> $local_profile
 }
-
 
 ## utility
 run() {
     if [ $is_dry ]; then
         echo "[dry run] $@"
     else
-        echo "[run] $@"
-        "$@"
+        if [ $is_verbose ];then
+            echo "[run] $@"
+        fi
+        eval "$@"
     fi
 }
 
@@ -105,6 +151,10 @@ green() {
     echo -n "[1;32m$1[0m"
 }
 
+gray() {
+    echo -n "[1;30m$1[0m"
+}
+
 fatal() {
     red "[fatal] "
     echo "$1"
@@ -118,6 +168,50 @@ warn() {
 info() {
     green "[info] "
     echo "$1"
+}
+
+debug() {
+    if [ $is_dry ] || [ $is_verbose ];then
+        gray "[debug] "
+        echo "$1"
+    fi
+}
+
+success() {
+    echo "[ $(green OK) ] $1"
+}
+
+failure() {
+    echo "[ $(red NG) ] $1"
+}
+
+judge() {
+    if [ $1 -eq 0 ];then
+        success $2
+    else
+        failure $2
+    fi
+}
+
+is_numeric() {
+    local value=$1
+    expr "$value" : "[0-9]*" > /dev/null
+    return $?
+}
+
+is_absolute() {
+    local path=$(echo $1)
+    [ "${path:0:1}" = "/" ]
+    return $?
+}
+
+resolve_path() {
+    local path=$1
+    if is_absolute $path; then
+        echo $(echo $path)
+    else
+        echo $(echo `pwd`/$path)
+    fi
 }
 
 # call main.
